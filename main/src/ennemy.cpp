6977faef87876d6974d0dc2824660c6d4341d08f
Twifus <moukira45@gmail.com>
2019-10-18 15:40:41 -0400

    Parenthesis fix Ennemy

--- main/src/ennemy.cpp
+++ main/src/ennemy.cpp
@@ -9,17 +9,17 @@ void Ennemy::Write(OutputStream& stream)
 	uint64_t t_y = static_cast<uint64_t>((position_y + 500.) * 1000);
 	uint64_t t_z = static_cast<uint64_t>((position_z + 500.) * 1000);
 
-	uint64_t posToSend = t_x << 40 + t_y << 20 + t_z;
+	uint64_t posToSend = (t_x << 40) + (t_y << 20) + t_z;
 
 	stream.Write<uint64_t>(posToSend);
 	
 	// Send quaternion
 	float valueToIgnore = -1;
 	uint8_t caseToIgnore = 0;
-	if (quaternion_a >= valueToIgnore) valueToIgnore = quaternion_a; caseToIgnore = 0;
-	if (quaternion_b >= valueToIgnore) valueToIgnore = quaternion_b; caseToIgnore = 1;
-	if (quaternion_c >= valueToIgnore) valueToIgnore = quaternion_c; caseToIgnore = 2;
-	if (quaternion_d >= valueToIgnore) valueToIgnore = quaternion_d; caseToIgnore = 3;
+	if (quaternion_a >= valueToIgnore) { valueToIgnore = quaternion_a; caseToIgnore = 0; }
+	if (quaternion_b >= valueToIgnore) { valueToIgnore = quaternion_b; caseToIgnore = 1; }
+	if (quaternion_c >= valueToIgnore) { valueToIgnore = quaternion_c; caseToIgnore = 2; }
+	if (quaternion_d >= valueToIgnore) { valueToIgnore = quaternion_d; caseToIgnore = 3; }
 	
 	uint32_t quatToSend = caseToIgnore;
 	uint8_t nextMove = 2;
