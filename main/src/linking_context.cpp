ddf28c2d1bf5aa9db6cb72b4e36bd1310dffd0e0
natoles <lindien.martin@gmail.com>
2019-10-18 14:05:56 -0400

    linking context done

--- main/src/linking_context.cpp
+++ main/src/linking_context.cpp
@@ -1,24 +1,44 @@
 #include "linking_context.hpp"
 
 
-void LinkingContext::Add(GameObject*)
+void LinkingContext::Add(GameObject* newGO)
 {
+	if (objToId.find(newGO) == objToId.end())
+	{
+		NetworkId newId = idToObj.rbegin->first + 1;
+		idToObj.insert({ newId, newGO });
+		objToId.insert({ newGO, newId });
+	}
 	return;
 }
 
 
-void LinkingContext::Remove(GameObject*)
+void LinkingContext::Remove(GameObject* gameObject)
 {
+	auto iteratorGO = objToId.find(gameObject);
+	if (iteratorGO != objToId.end())
+	{
+		objToId.erase(iteratorGO);
+		idToObj.erase(objToId[gameObject]);
+	}
 	return;
 }
 
 
-std::optional<NetworkId> LinkingContext::GetId(GameObject*)
+std::optional<NetworkId> LinkingContext::GetId(GameObject* gameObject)
 {
+	if (objToId.find(gameObject) != objToId.end())
+	{
+		return objToId[gameObject];
+	}
 	return {};
 }
 
-std::optional<GameObject*> LinkingContext::GetGameObject(NetworkId)
+std::optional<GameObject*> LinkingContext::GetGameObject(NetworkId nId)
 {
+	if (idToObj.find(nId) != idToObj.end())
+	{
+		return idToObj[nId];
+	}
 	return {};
 }
